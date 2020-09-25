521a7693d0ea071b273f933e8a440bd266d309c3
Yigitokkoo <yigitakkasbjk@hotmail.com>
2020-09-25 18:55:59 +0300

    Dice Roller Finished

---
+++ app/src/main/java/com/yigitakkas/dice_roller/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.yigitakkas.dice_roller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        roll_button.text = "Let's Roll"
+        roll_button.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val randomInt = Random().nextInt(6) + 1
+        val drawableResource = when (randomInt) {
+            1-> R.drawable.dice_1
+            2-> R.drawable.dice_2
+            3-> R.drawable.dice_3
+            4-> R.drawable.dice_4
+            5-> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        dice_image.setImageResource(drawableResource)
+    }
+}
